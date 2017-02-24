d87f460ba0973b7c89c13398017a1c7fd3377a13
Laurent Milne <lmilne360@gmail.com>
2017-02-24 15:17:37 -0500

    created crud functions in ExerciseService

--- app/assets/javascripts/ng-app/exercises/ExerciseService.js
+++ app/assets/javascripts/ng-app/exercises/ExerciseService.js
@@ -3,6 +3,23 @@ function ExerciseService($http) {
     this.getAll = function() {
         return $http.get('/exercises');
     };
+
+    this.getExercise = function(id){
+      return $http.get(`/exercises/${id}`);
+    };
+
+    this.createExercise = function(exerciseData){
+      return $http.post('/exercises', {exerciseData})
+    };
+
+    this.updateExercise = function(exerciseData) {
+      return $http.patch(`/exercises/${exerciseData.id}`,{exerciseData});
+    };
+
+    this.deleteExercise = function(id) {
+      return  $http.delete(`/exercises/${id}`);
+    };
+    
 }
 angular
   .module('app')
