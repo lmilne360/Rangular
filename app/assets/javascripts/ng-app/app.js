40f363ab29b271fa751cf94ba000ef906a1450a1
Laurent Milne <lmilne360@gmail.com>
2017-02-24 13:05:57 -0500

    templating done

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -14,6 +14,28 @@ angular
                 url: '/',
                 templateUrl: 'exercisesIndex.html',
                 controller: 'exCtrl as ctrl'
+            })
+
+            //
+            .state('dashboard', {
+            abstract: true,
+            url: '/dashboard',
+            templateUrl: 'layout.html'
+        })
+            // the default route when someone hits dashboard
+            .state('dashboard.one', {
+                url: '',
+                templateUrl: 'tab1.html'
+            })
+            // this is /dashboard/two
+            .state('dashboard.two', {
+                url: '/two',
+                templateUrl: 'tab2.html'
+            })
+            // this is /dashboard/three
+            .state('dashboard.three', {
+                url: '/three',
+                templateUrl: 'tab3.html'
             });
 
         // default fall back route
