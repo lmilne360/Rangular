2720f19eabafafbeb489e16a0ada551a8d305989
Laurent Milne <lmilne360@gmail.com>
2017-02-23 23:44:36 -0500

    basic angular setup

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,9 @@
 angular
-    .module('app', [])
+    .module('app', ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('exercises', {
+                templateUrl: 'angular/templates/exercises.html',
+                controller: 'UserController'
+            });
+    });
