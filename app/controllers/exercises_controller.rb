3b0059eb39dc23130aa53a2faf84da3f9d01e1d0
Laurent Milne <lmilne360@gmail.com>
2017-02-24 20:04:45 -0500

    Added pry and edited rails exercise controller

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,12 +1,12 @@
 class ExercisesController < ApplicationController
-
+before_action :set_exercise, only: [:show, :update, :destroy]
   def index
     @exercises = Exercise.all
     render json: @exercises
   end
 
   def show
-
+    render json: @exercise
   end
 
   def new
@@ -27,5 +27,9 @@ class ExercisesController < ApplicationController
 
   def destroy
 
+  end
+
+  def set_exercise
+    @exercise = Exercise.find(params[:id])
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+#require 'pry'
 
 require 'rails/all'
 
