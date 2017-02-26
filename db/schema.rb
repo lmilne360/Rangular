30d0b9189a3d0ee45be2ec29fdf2d1e387f39c44
Laurent Milne <lmilne360@gmail.com>
2017-02-25 20:45:11 -0500

    Added reps and weight to exercise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224035339) do
+ActiveRecord::Schema.define(version: 20170226014320) do
 
   create_table "exercises", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "reps"
+    t.integer  "weight"
   end
 
 end
